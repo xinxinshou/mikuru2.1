a436dcdfd99ef47e432f1ec8e7830406bab68a91
xinxinshou <xinxinshou@126.com>
2022-01-20 00:58:03 +0800

    yes

--- code_get.py
+++ code_get.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 from time import sleep 
 
+
 #获取公司简介和岗位要求
 option = webdriver.ChromeOptions()
 #prefs = {"profile.managed_default_content_settings.images": 2}
@@ -11,12 +12,13 @@ option = webdriver.ChromeOptions()
 url=input('请输入岗位网址（目前仅支持51招聘和boss直聘网站）')
 driver = webdriver.Chrome(options=option)
 driver.get(url)
+info={}
 #51招聘
 #url='https://jobs.51job.com/hefei-gxq/121766663.html?s=sou_sou_soulb&t=0'（示例）
 company_name=''
 job_bonus=''
-sleep(10)
-soup=BeautifulSoup(driver.page_source,'lxml')
+sleep(15)
+'''soup=BeautifulSoup(driver.page_source,'lxml')'''
 if('51job' in url):
     '''job_require=soup.find(class_='bmsg job_msg inbox')
     job_requirements_list=[]
@@ -25,24 +27,102 @@ if('51job' in url):
             job_requirements_list.append(i.string.strip())
         if((i.name is 'div')or(i is None)):
             break'''
-    job_requirements=driver.find_element_by_class_name('tBorderTop_box').text.replace('\n','')
-    company_info_list=[i.string for i in soup.find(class_='tmsg inbox').contents if i.string is not None]
-    company_name=str(soup.find(class_='catn').contents[0])
-    company_info=''.join(company_info_list).replace(" ",'')
-    #job_requirements=''.join(job_requirements_list)
-    job_bonus=driver.find_element_by_class_name("jtag").text.replace("\n",'、')
-elif('zhipin' in url):
+    '''company_name=str(soup.find(class_='catn').contents[0])'''
+    try:
+        title=driver.find_elements_by_css_selector('/html/body/div[3]/div[2]/div[2]/div/div[1]/h1').text
+    except:
+        title=''
+    info['title']=title
+    try:
+        salary_range=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[1]/strong').text
+    except:
+        salary_range=''
+    info['salary_range']=salary_range
+    try:
+        location=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[3]/div[2]/div/a').get_attribute('onclick').split('\'')[3]
+    except:
+        location=''
+    info['location']=location
+    try:
+        department=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[3]/div[3]/div').text[5:].split('\n')[0]
+    except:
+        department=''
+    info['department']=department
+    try:
+        benefit=driver.find_element_by_class_name("jtag").text.replace("\n",'、')
+    except:
+        benefit=''
+    info['benefit']=benefit
+    try:
+        required_education=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[1]/p[2]').text.split('|')[2].split()[0]
+    except:
+        required_education=''
+    info['required_education']=required_education
+    try:
+        required_experience=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[1]/p[2]').text.split('|')[1].split()[0]
+    except:
+        required_experience=''
+    info['required_experience']=required_experience
+    try:
+        industry=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[4]/div[1]/div[2]/p[3]').text
+    except:
+        industry=''
+    info['industry']=industry
+    try:
+        fuction=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[3]/div[1]/div/div[1]/p[1]/a').text
+    except:
+        fuction=''
+    info['fuction']=fuction
+    if len(driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[4]/div[1]/div[1]/a').find_elements_by_tag_name('img'))==0:
+        has_company_logo=0
+    else:
+        has_company_logo=1
+    try:
+        description_origin=[i for i in driver.find_element_by_class_name('bmsg').text.replace(' ','').split('\n') if i!='']
+    except:
+        description_origin=''
+    if '岗位职责' in description_origin:
+        index1=description_origin.index('岗位职责')
+    elif '工作职责' in description_origin:
+        index1=description_origin.index('工作职责')
+    elif '岗位职责：' in description_origin:
+        index1=description_origin.index('岗位职责：')
+    elif '工作职责：' in description_origin:
+        index1=description_origin.index('工作职责：')
+    elif 'Responsibility:' in description_origin:
+        index1=description_origin.index('Responsibility:')
+    else:
+        index1=0
+    if '任职要求' in description_origin:
+        index2=description_origin.index('任职要求')
+    elif '任职资格' in description_origin:
+        index2=description_origin.index('任职资格')
+    elif '任职要求：' in description_origin:
+        index2=description_origin.index('任职要求：')
+    elif '任职资格：' in description_origin:
+        index2=description_origin.index('任职资格：')
+    elif 'Requirements::' in description_origin:
+        index2=description_origin.index('Requirements:')
+    if index2>index1:
+        description=description_origin[index1:]
+        requirements=description_origin[index2:index1-1]
+    else:
+        description=description_origin[index2:]
+        requirements=description_origin[index1:index2-1]
+    info['description']=''.join(description[1:])
+    info['requirements']=''.join(requirements[1:])
+'''elif('zhipin' in url):
     job_bonus='、'.join([i.string.strip() for i in soup.find('div',class_='job-tags') if len(i.string)>1])
     job_requirements=''.join([i.string.replace('\n','').strip() for i in soup.find('div',class_='text').contents if(i.string is not None)])
     company_name=soup.find(string='工商信息').parent.parent.find('div',class_='name').string
-    t=soup.find(string='公司介绍').parent.parent.find('div',class_='text')
-print('公司名称:'+company_name)
-print('福利待遇:'+job_bonus)
-print('岗位需求:'+job_requirements)
-print('公司信息:'+company_info)
+    t=soup.find(string='公司介绍').parent.parent.find('div',class_='text')'''
+
+
+print(info)
+#print('公司信息:'+company_profile)
 #通过上企查查判断企业是否为失信企业、有营业执照
 #判断经营状况（未实现）
-url2='https://www.qcc.com/web/search?key='+company_name
+'''url2='https://www.qcc.com/web/search?key='+company_name
 driver.get(url2)
 while('405' in driver.current_url):
     driver.get(url2)
@@ -57,4 +137,5 @@ try:
     else:
         print('否')
 except:
-    print('企查查中未能找到该企业的相关信息')
+    print('企查查中未能找到该企业的相关信息')'''
+        
